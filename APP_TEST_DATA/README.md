bf0c1baacb08a92f19b29aefa270e8a3e0f55bca
mannulus <pipe_050401@hotmail.com>
2023-10-04 16:05:49 -0500

    Add aplications for testing data device

---
+++ APP_TEST_DATA/README.md
@@ -0,0 +1,9 @@
+# APP PRUEBAS DATOS DISPOSITIVOS
+
+El aplicacion diseñada en html y Javascript es utilizada para testear el sistema de comunicacion
+de los dispositivos con el servidor MQTT, el cual el back utiliza para obtener los datos y almacenarlos
+en mongoDb.
+
+## USO
+Se debe abir el html como una pagina en el navegador, dentro se debe configurar la conexion con el servidor
+MQTT.
---
+++ APP_TEST_DATA/index.js
@@ -0,0 +1,179 @@
+function println(msg) {
+    document.getElementById("messages").innerHTML += "<span class='log'>" + msg + "</span><br>";
+}
+
+// const {faker} = require("@faker-js/faker");
+var client;
+
+function startConnect() {
+
+    clientID = "clientID - " + parseInt(Math.random() * 100);
+    // protocol = document.getElementById("protocol").value;
+    host = document.getElementById("host").value;
+    port = document.getElementById("port").value;
+    userId = document.getElementById("username").value;
+    passwordId = document.getElementById("password").value;
+
+    document.getElementById("messages").innerHTML += `<span> Connecting to ${host} on port ${port} </span><br>`;
+    document.getElementById("messages").innerHTML += `<span> Using the client Id ${clientID} </span><br>`;
+
+    client = new Paho.MQTT.Client(host, Number(port), clientID);
+
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+
+    client.connect({
+        onSuccess: onConnect
+//        userName: userId,
+        //       passwordId: passwordId
+    });
+
+
+}
+
+
+function onConnect() {
+    topic = document.getElementById("topic_s").value;
+
+    document.getElementById("messages").innerHTML += "<span> Subscribing to topic " + topic + "</span><br>";
+
+    client.subscribe(topic);
+}
+
+function onConnectionLost(responseObject) {
+    document.getElementById("messages").innerHTML += "<span> ERROR: Connection is lost.</span><br>";
+    if (responseObject != 0) {
+        document.getElementById("messages").innerHTML += "<span> ERROR:" + responseObject.errorMessage + "</span><br>";
+    }
+}
+
+function onMessageArrived(message) {
+    console.log("OnMessageArrived: " + message.payloadString);
+    document.getElementById("messages").innerHTML += "<span> Topic:" + message.destinationName + "| Message : " + message.payloadString + "</span><br>";
+}
+
+function startDisconnect() {
+    client.disconnect();
+    document.getElementById("messages").innerHTML += "<span> Disconnected. </span><br>";
+
+
+}
+
+function publishMessage(msg) {
+    // msg = document.getElementById("Message").value;
+    // const deviceid = document.getElementById("deviceUUID").value;
+
+    Message = new Paho.MQTT.Message(msg);
+    //topico donde el gateway publica los mensajes de los dispositivos y el backend los recibe
+    Message.destinationName = document.getElementById("topic_s").value;
+    MessageUser = new Paho.MQTT.Message(msg);
+
+    MessageUser.destinationName = ("device-" + document.getElementById("deviceUUID").value);
+
+    //se envia el mensaje al backend y al usuario.
+    client.send(Message);
+    client.send(MessageUser);
+    println("Message to topic " + topic + " is sent");
+}
+
+let tiempoInicial;
+
+function generarRegistro(keys, min, max, deviceid) {
+    console.log(tiempoInicial, window.tiempoInicial)
+    if (deviceid === undefined) {
+        deviceid = document.getElementById("deviceUUID").value;
+    }
+    if (min === undefined) {
+        min = Number(document.getElementById("min").value);
+    }
+    if (max === undefined) {
+        max = Number(document.getElementById("max").value);
+    }
+    if (keys === undefined) {
+        keys = document.getElementById("keys").value.split(",");
+    }
+    const topic = ("device-" + deviceid);
+
+    const intervalo = document.getElementById("intervalo").value;
+    tiempoInicial = tiempoInicial.add(1, intervalo);
+
+    let values = {};
+    keys.forEach(key => {
+        values[key] = faker.random.number({min: min, max: max, precision: 1});
+    });
+    return JSON.stringify({
+        deviceUUID: deviceid,
+        topic: topic,
+        timeStamp: tiempoInicial.toDate(),
+        values: values,
+        status: 'OK',
+        alert: false,
+    });
+}
+
+async function generateData(type) {
+    const count = Number(document.getElementById("count").value);
+    //tiempo de espera entre cada registro
+    const timeout = document.getElementById("intervalo").value;
+
+    const fechaDesde = document.getElementById("fechaDesde").value;
+    tiempoInicial = moment(fechaDesde) ?? moment();
+    if (count === null || timeout === null) {
+        println("Debe llenar los campos");
+        return;
+    }
+    println("Generando " + count + " registros cada " + timeout + " milisegundos");
+    for (let i = 0; i < count; i++) {
+        if (type === "co2") {
+            publishMessage(generarRegistro(["C02 ppm"], 1500, 3000, 1));
+        } else if (type === "energia") {
+            publishMessage(generarRegistro(["Centic", "CT", "LP"], 600, 1400, 2));
+        } else if (type === "temp") {
+            publishMessage(generarRegistro(["Asignadas", "Prestadas", "NO prestadas"], 0, 30, 3));
+        } else if (type === "ruido") {
+            publishMessage(generarRegistro(["decibeles"], 30, 90, 4));
+        } else{
+            publishMessage(generarRegistro());
+        }
+    }
+}
+
+function clearLog() {
+    document.getElementById("messages").innerHTML = "";
+}
+
+// Generar una serie de datos correlacionados linealmente
+function generarDatosCorrelacionados() {
+    const datos = [];
+    // Ejemplo de uso:
+    const tamanoMuestra = Number(document.getElementById("count").value);
+    const pendiente = 2; // Pendiente de la línea
+    const intercepto = 10; // Intercepción en el eje y
+    const ruido = 5; // Amplitud del ruido aleatorio
+    for (let i = 0; i < tamano; i++) {
+        const x = i;
+        const y = pendiente * x + intercepto + (Math.random() - 0.5) * ruido;
+        //datos.push({ x, y });
+        let values = {};
+        keys.forEach(key => {
+            values[key] = pendiente * x + intercepto + (Math.random() - 0.5) * ruido;
+        });
+        msj = JSON.stringify({
+            deviceUUID: deviceid,
+            topic: topic,
+            timeStamp: tiempoInicial.toDate(),
+            values: values,
+            status: 'OK',
+            alert: faker.random.boolean(),
+        });
+        publishMessage(msj);
+    }
+    return datos;
+}
+
+console.log(document.getElementById("fechaDesde"))
+$(document).ready(function () {
+   console.log("ready")
+    $("#fechaDesde").val(moment().format("YYYY-MM-DDTHH:mm:ss"));
+    //document.getElementById("fechaDesde").value = moment().format("YYYY-MM-DDTHH:mm:ss");
+});
